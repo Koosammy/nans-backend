72d26f1a392f8d5ac183d3e509797ab36faa21e9
Koosammy <87281182+Koosammy@users.noreply.github.com>
2025-05-27 00:10:15 +0000

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: 'https://nans-dashboard.netlify.app/login', 
+  origin: 'https://nans-dashboard.netlify.app', 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'x-auth-token'],
   credentials: true
